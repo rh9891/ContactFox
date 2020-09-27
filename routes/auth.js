2861aab30790722077207293a9b7a917524b5abd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-27 10:43:41 -0400

    Set up the login and auth route for the logged in user.

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,15 @@
 // Login and authentication. Route to check the logged in user.
+const express = require("express");
+const router = express.Router();
+
+// GET route with "api/auth" endpoint that gets the logged in user. Private access.
+router.get("/", (req, res) => {
+    res.send("Get logged in user.");
+});
+
+// POST route with "api/auth" endpoint that authorizes the user and gets the token. Public access.
+router.get("/", (req, res) => {
+    res.send("Log in user.");
+});
+
+module.exports = router;
