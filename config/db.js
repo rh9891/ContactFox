b3c059c048f4e3301e40af4fc34a94d0fdf6d506
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-27 13:06:57 -0400

    Refactored the code tto make the database connection.

--- config/db.js
+++ config/db.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-const connectDB = () => {
-    mongoose.connect(db, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    }).then(() => console.log("MongoDB Connected.")).catch(err => {
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB Connected...")
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
-    });
+    }
 };
 
 module.exports = connectDB;
