adfc630ae1a52562d1b80614490a8ec5a50ebbab
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-27 10:39:04 -0400

    Set up the register route for the user.

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,10 @@
 // Register route.
+const express = require("express");
+const router = express.Router();
+
+// POST route with "api/users" endpoint that registers a user. Public access.
+router.post("/", (req, res) => {
+    res.send("Register a user.");
+});
+
+module.exports = router;
