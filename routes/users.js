867c3837975c8c5c4e128f1c11c4049e4c898a0f
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-29 23:36:19 -0400

    Completed the backend registration route for the new user. Hashed the password using bcrypt for when making a new user. Used Postman to verify that register route was working as expected. Checked Atlas to verify that new user was created and saved in database. All is working as expected.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 // Register route.
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
 // Brings in the User model, so that it can be used in the routes.
@@ -12,14 +13,38 @@ router.post("/", [
     check("name", "Please enter a name.").not().isEmpty(),
     check("email", "Please enter a valid email address.").isEmail(),
     check("password", "Please enter a password with 8 or more characters.").isLength({ min: 8 })
-], (req, res) => {
+], async (req, res) => {
     // Errors object that is sent through the backend and sends the user a 400 error if the requirements for the user model are not met.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    // Approval message if the requirements are met. 
-    res.send("Approved.")
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: "User already exists. Please choose another email address." })
+        }
+
+        user = new User({
+            name,
+            email, 
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send("New user has been saved in database.")
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error.");
+    }
 });
 
 module.exports = router;
