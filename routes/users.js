3953afd7e4f810ebd777c0b4cd3870e7b5bcd6cd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-29 22:49:52 -0400

    Brought in the User model, so that it can be used in the routes.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const router = express.Router();
 
+// Brings in the User model, so that it can be used in the routes.
+const User = require("../models/User");
+
 // POST route with "api/users" endpoint that registers a user. Public access.
 router.post("/", (req, res) => {
     res.send("Register a user.");
