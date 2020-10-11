8f28711353e5229f743566e00d805ece756c4d38
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-10 23:11:08 -0400

    Set up the private route to add update contact. Checked the update route and varying authorization instances with Postman and application is running as expected.

--- routes/contacts.js
+++ routes/contacts.js
@@ -52,8 +52,37 @@ router.post(
 );
 
 // PUT route with "api/contacts/:id" endpoint to update contact. Private access.
-router.put("/:id", (req, res) => {
-  res.send("Update contact.");
+router.put("/:id", auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Checks to see if the fields are submitted by initializing an object and then adds to the contact fields.
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found." });
+
+    // Verifies that logged in user is the owner of contact.
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User is not authorized." });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // DELETE route with "api/contacts/:id" endpoint to update contact. Private access.
