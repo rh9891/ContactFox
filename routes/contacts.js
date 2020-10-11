aca2cee188df930a907fa35abe1bb0c7b0324b82
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-10 23:21:09 -0400

    Set up the private route to delete a contact. Checked the delete route by deleting two users with Postman and application is running as expected.

--- routes/contacts.js
+++ routes/contacts.js
@@ -86,8 +86,24 @@ router.put("/:id", auth, async (req, res) => {
 });
 
 // DELETE route with "api/contacts/:id" endpoint to update contact. Private access.
-router.delete("/:id", (req, res) => {
-  res.send("Delete contact.");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: "Contact not found." });
+
+    // Verifies that logged in user is the owner of contact.
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User is not authorized." });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Contact has been removed." });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
