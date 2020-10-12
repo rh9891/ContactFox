8d78487793f226d7c0933be356cbeec992ea8507
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-12 15:44:45 -0400

    Set up the router and completed the functionality of the navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">My App</div>;
+  return (
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { GiFoxHead } from "react-icons/gi";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <GiFoxHead />
+        &nbsp;
+        {title}
+      </h1>
+      <ul>
+        <l1>
+          <Link to="/">Home</Link>
+        </l1>
+        <l1>
+          <Link to="/about">About</Link>
+        </l1>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: "Contact Fox",
+};
+
+export default Navbar;
