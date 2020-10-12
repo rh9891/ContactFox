8ce7e570af840cf9ba47217b150a31975de4a800
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-12 18:19:03 -0400

    Updated CSS again. Completed the functionality to add a contact to the user interface, but it isn't a permanent save as the contact gets deleted upon refresh since the data is currently hardcoded. Am working on getting the CRUD functionality of the application down before calling on the database.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
 
 const Home = () => {
   return (
     <div className="grid-2">
-      <div>{/* ContactForm */}</div>
+      <div>
+        <ContactForm />
+      </div>
       <div>
         <Contacts />
       </div>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -42,6 +42,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add a contact.
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete a contact.
 
@@ -59,6 +63,7 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
