f3022714a680957b9d55fc9acbc8120a13bd96cc
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-13 07:19:48 -0400

    Created the functionality for the alert context/component. Added length parameter to input form for passwords, so that message is automated.

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,33 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 import "./App.css";
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment className="App">
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment className="App">
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -15,7 +20,13 @@ const Register = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log("Register submit.");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields.", "danger");
+    } else if (password !== password2) {
+      setAlert("Passwords do not match.", "danger");
+    } else {
+      console.log("Register submit.");
+    }
   };
 
   return (
@@ -39,6 +50,7 @@ const Register = () => {
             name="password"
             value={password}
             onChange={onChange}
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -48,6 +60,7 @@ const Register = () => {
             name="password2"
             value={password2}
             onChange={onChange}
+            minLength="6"
           />
         </div>
         <input
