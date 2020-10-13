2aa16e7e8621d85bfa35541a203f20d821d6da3d
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-13 00:25:50 -0400

    Created the form/component for when logging in a returning user.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -21,6 +22,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = user;
+
+  const onChange = (event) =>
+    setUser({ ...user, [event.target.name]: event.target.value });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("Login submit.");
+  };
+
+  return (
+    <div className="form-container">
+      <h1>
+        <span className="text-primary">Login</span> Account
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Login"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
