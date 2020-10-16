2a092e83ee8b4acd1f38c8820ab214019d7d4939
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-10-15 21:27:30 -0400

    Completed functionality to delete contacts from the application and database.

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -66,8 +66,20 @@ const ContactState = (props) => {
   };
 
   // Function to delete a contact.
-  const deleteContact = (id) => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+  const deleteContact = async (id) => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
   };
 
   // Function to clear all contacts from the
